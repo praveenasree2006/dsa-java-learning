800fd54537853b07cdb922a213360754f67ff7ea
PRAVEENA SREE NB <nbpraveenasree@gmail.com>
2026-09-21 10:49:25 +0530

    Time: 134 ms (38.68%), Space: 18.3 MB (85.71%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        r = 0
+        c = len(matrix[0]) - 1
+        while r < len(matrix) and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+        return False
+        
