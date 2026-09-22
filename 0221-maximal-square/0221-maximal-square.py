bd055f60e6070081d2dbdb76ecd844eab9cdfac1
PRAVEENA SREE NB <nbpraveenasree@gmail.com>
2026-09-22 09:29:57 +0530

    Time: 139 ms (95.48%), Space: 26 MB (40.35%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        
+        # Initialize the DP table
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0] * cols for _ in range(rows)]
+        max_side = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                    max_side = max(max_side, dp[i][j])
+        
+        return max_side * max_side
+
+        
