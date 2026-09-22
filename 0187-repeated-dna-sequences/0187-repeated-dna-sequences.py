fd320edb06df550d0bc9ba09c095e61055ab60aa
PRAVEENA SREE NB <nbpraveenasree@gmail.com>
2026-09-22 09:31:41 +0530

    Time: 973 ms (33.85%), Space: 34.7 MB (6.34%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        map = {}
+        result = []
+
+        for i in range(len(s) - 9):
+            substring = s[i:i + 10]
+            if substring in map:
+                if map[substring] == 1:
+                    result.append(substring)
+                map[substring] += 1
+            else:
+                map[substring] = 1
+        
+        return result
+        
