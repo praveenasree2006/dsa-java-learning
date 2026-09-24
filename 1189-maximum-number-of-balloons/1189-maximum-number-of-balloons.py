1660f1758f5650227a4005ef7746e61e63f1f7fd
PRAVEENA SREE NB <nbpraveenasree@gmail.com>
2026-09-24 09:33:41 +0530

    Time: 12 ms (7.2%), Space: 12.3 MB (84.41%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        f = Counter(text)
+        return min(f["b"], f["a"], f["l"] >> 1, f["o"] >> 1, f["n"])
+        
